261591f7c1a40a4f92c82cb11b4770a28b3661f8
kotyabuchi <kotyabuchi@gmail.com>
2024-10-04 23:03:07 +0900

    CHANGE: tailwindのコンフィグにプラグイン追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from '@nextui-org/react';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,6 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -13,8 +15,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
+        'user-list': 'repeat(auto-fill, minmax(120px, 1fr))',
+        'auto-fit': 'repeat(auto-fit, minmax(200px, 1fr))',
+      },
+      boxShadow: {
+        'soft-sm':
+          '0px 0px 5px 0px rgb(0 0 0 / 0.02), 0px 2px 10px 0px rgb(0 0 0 / 0.06)',
+        'soft-md':
+          '0px 0px 15px 0px rgb(0 0 0 / 0.03), 0px 2px 30px 0px rgb(0 0 0 / 0.08)',
+        'soft-lg':
+          '0px 0px 30px 0px rgb(0 0 0 / 0.04), 0px 30px 60px 0px rgb(0 0 0 / 0.12)',
+      },
     },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [
+    nextui(),
+    require('@tailwindcss/line-clamp'),
+  ],
 };
 export default config;
