540ea02148beee66d15d2cc77de2f002f5c081fc
kotyabuchi <kotyabuchi@gmail.com>
2024-10-05 12:09:33 +0900

    CHANGE: 一部依存関係を変更

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
 import { nextui } from '@nextui-org/react';
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       gridTemplateColumns: {
         'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
@@ -31,9 +31,6 @@ const config: Config = {
     },
   },
   darkMode: 'class',
-  plugins: [
-    nextui(),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [nextui()],
 };
 export default config;
